e03dba7b2c1730dae69c8a5af1d60279c2c3fe1d
yortyy <lan30san@gmail.com>
2023-04-05 14:11:39 -0700

    4-4-23 1.0.1
    Added Particle System
    Added shot meter and shot skill system
    Added missing shots
    Added shot in or not

--- Assets/bassetball.cs
+++ Assets/bassetball.cs
@@ -2,14 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using static UnityEngine.GraphicsBuffer;
 
 public class bassetball : MonoBehaviour
 {
     public Transform target;
     private GameObject player;
     private bool shoot;
-    private bool playerholding;
+    public bool playerholding;
     private Rigidbody bbrb;
+    public int shotpercent;
+    private int shotresultnum;
+    public bool shotresult;
     float count = 0.0f;
     void Awake()
     {
@@ -28,19 +32,36 @@ public class bassetball : MonoBehaviour
             Debug.Log("KONNECT");
         }
     }
-    public void shooter(InputAction.CallbackContext value)
+    public void shooter(int shootingskill, int smeter)
     {
-        if(playerholding)
+        bbrb.detectCollisions = true;
+        playerholding = false;
+        shotpercent = 100 - (shootingskill * 2) - (smeter * 5); //out of 100, green - 95, slight early - 80, slight late - 70, early - 50, late - 40, very early/late - 10, nah - 0
+        shotresultnum = (shotpercent - Random.Range(0, 100));
+        if(shotresultnum > 0)
         {
-            bbrb.detectCollisions = true;
-            playerholding = false;
-            shoot = true;
+            //shots good
+            shotresult = true;
         }
-
+        else if(shotresultnum < 0)
+        {
+            //shots bad
+            shotresult = false;
+        }
+        else if (shotresultnum == 0)
+        {
+            //special shot animation
+            shotresult = true;
+        }
+        Debug.Log("Shotresult: " + shotresult + " | Shotresultnum: " + shotresultnum + " | Shotpercentage: " + shotpercent);
+        Debug.Log(shotresultnum);
+        shoot = true;
     }
 
     Vector3 startpoint;
     Vector3 archpoint;
+    Vector3 targetpoint;
+    Vector3 offset;
     bool setarch;
     void FixedUpdate()
     {
@@ -51,13 +72,31 @@ public class bassetball : MonoBehaviour
             {
                 archpoint = transform.position + (target.position - transform.position) / 2 + Vector3.up * 8.0f;
                 startpoint = transform.position;
+                if (!shotresult)
+                {
+                    if(1 == Random.Range(1, 2))
+                    {
+                        offset.x = Random.Range(0.5f, 1f);
+                        offset.z = Random.Range(-1f, -0.5f);
+                    }
+                    else
+                    {
+                        offset.x = Random.Range(-1f, -0.5f);
+                        offset.z = Random.Range(-1f, -0.5f);
+                    }
+                    targetpoint = (target.position + offset);
+                }
+                else
+                {
+                    targetpoint = target.position;
+                }
                 setarch = true;
             }
             count += 1f * Time.deltaTime;
 
             //Vector3 archpoint = new Vector3((target.position.x - transform.position.x)/2, (target.position.y - transform.position.y)/2 + 3, (target.position.z - transform.position.z)/2);
             Vector3 m1 = Vector3.Lerp(startpoint, archpoint, count);
-            Vector3 m2 = Vector3.Lerp(archpoint, target.position, count);
+            Vector3 m2 = Vector3.Lerp(archpoint, targetpoint, count);
             transform.position = Vector3.Lerp(m1, m2, count);
         }
         else if (setarch)
--- Assets/playermovement.cs
+++ Assets/playermovement.cs
@@ -8,13 +8,20 @@ public class playermovement : MonoBehaviour
 {
 
     private Rigidbody rb;
+    private ParticleSystem ps;
+    public GameObject basketballobj;
+    private bassetball bballscript;
     private Vector2 movementVector;
     private bool jumpon;
     public float mscale = 5f;
     public float jscale = 5f;
+    public int shootingskill;
+    public int smeter;
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        ps = GetComponent<ParticleSystem>();
+        bballscript = basketballobj.GetComponent<bassetball>();
     }
 
     public void moveinp(InputAction.CallbackContext movementValue)
@@ -22,11 +29,27 @@ public class playermovement : MonoBehaviour
         movementVector = movementValue.ReadValue<Vector2>();
 
     }
-    public void jumpinp(InputAction.CallbackContext movementValue)
+    public void jumpinp(InputAction.CallbackContext value)
     {
         rb.AddForce(0, jscale * mscale, 0, ForceMode.Impulse);
         Debug.Log("Jump");
     }
+    public void shootimp(InputAction.CallbackContext value)
+    {
+        if(bballscript.playerholding)
+        {
+            //check for skill and shotmeter then put into shooter()
+            //for this example, shootingskill is 1 (excellent), smeter is green (1)
+            // 1 - green, 4 - searly, 6 - slate, 10 - early, 12 - late, 18 - very late/early, 20 - nah
+            // 0 - excellent skill -> 10 (worst), minus from 80 -> 60
+            if(smeter == 1)
+            {
+                ps.Play();
+            }
+            bballscript.shooter(shootingskill, smeter);
+        }
+    }
+
     void FixedUpdate()
     {
         // Add a forward force
@@ -34,4 +57,5 @@ public class playermovement : MonoBehaviour
         //Debug.Log(movementVector);
 
     }
+
 }
